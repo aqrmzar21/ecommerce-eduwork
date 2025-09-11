1ee6440f381e38a8ebeea16006f04c2abb99e9b3
aqrmzar21 <aqramzoombar@gmail.com>
2025-09-11 15:46:22 +0800

    firts install brezee laravel

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-// use Inertia\Inertia;
-use App\Http\Controllers\Contoh;
-use App\Http\Controllers\ProductController;
 
-// Route::get('/', function () {
-//     return Inertia::render('welcome');
-// })->name('home');
-// Route::get('/cart', function () {
-//     return "ini adalah route cart";
-// });
-
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/cart', [HomeController::class, 'cart']);
-
-Route::get('/products', function () {
-    return "ini adalah route produts";
-});
-Route::get('/checkout', function () {
-    return "ini adalah route checkout";
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// langsung pakai resource karena sudah ada controller dari perintah -r
-Route::resource('products', ProductController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
