4b8ad84502766da558f0e7e5fd9af721f6f36316
aqrmzar21 <aqramzoombar@gmail.com>
2025-09-11 10:33:07 +0800

    add konsional alert

--- resources/views/contoh.blade.php
+++ resources/views/contoh.blade.php
@@ -5,8 +5,12 @@
     {{-- Coba kompoenen alert --}}
   <x-alert>
     {{-- Coba kondisional if-else --}}
-    <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50" role="alert">
-      <span class="font-medium">Berhasil!</span> Data telah disimpan.
+    <div class="p-4 mb-4 text-sm text-gray-800 rounded-lg bg-green-50" role="alert">
+      @if($ifLogin)
+      <span class="font-medium text-green-500">Berhasil!</span> Data telah disimpan.
+      @else
+      <span class="font-medium text-red-500">Gagal!</span> menyimpan data.
+      @endif
     </div>
   </x-alert>
 
