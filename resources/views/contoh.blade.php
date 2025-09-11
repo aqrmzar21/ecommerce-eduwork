c24e4468bda190654505c9efe2a5d84c0d5d5ed8
aqrmzar21 <aqramzoombar@gmail.com>
2025-09-11 09:01:36 +0800

    second commit

--- resources/views/contoh.blade.php
+++ resources/views/contoh.blade.php
@@ -1,16 +1,3 @@
-{{-- <!DOCTYPE html>
-<head>
-  <title>Contoh</title>
-</head>
-<body>
-  <h1>{{ $judul }}</h1>
-  <ul>
-    @foreach ($produk as $item)
-    <li>{{ $item }}</li>
-    @endforeach
-  </ul>
-</body>
-</html> --}}
 @extends('layout')
 @section('title', 'Contoh')
 @section('content')
