dc82689b0a74924af21ce21edf0a65795a71edf7
aqrmzar21 <aqramzoombar@gmail.com>
2025-09-11 14:18:27 +0800

    menambhakan model Produk dan Kategori

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Models;
+use App\Models\ProductCategories;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     //
+    public function category() {
+        return $this->belongsTo(ProductCategories::class);
+    }
+    
 }
